4f111b120227a150daad81e50332ad821808a0ef
Dawid <infarot@gmail.com>
2019-01-20 19:42:15 +0100

    Initial commit

---
+++ src/main/java/com/dawid/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.dawid.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@GetMapping(value="/")
+	public String showHome(){
+		return ("home");
+	}
+}
